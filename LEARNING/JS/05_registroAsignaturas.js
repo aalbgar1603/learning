8c1133b301667cc7877b9bc10dfcc98b77915299
blkshhh <er.miguel.ag@gmail.com>
2023-05-11 08:30:27 +0200

    11/05/23 - voy a seguir en la tienda. no consigo que guarde las asignaturas de cada alumno

--- LEARNING/JS/05_registroAsignaturas.js
+++ LEARNING/JS/05_registroAsignaturas.js
@@ -1,8 +1,8 @@
 const obtenerInformacion = (materia)=>{
     materias = {
-        deporte: ["pedro","juan","perico","manolo"],
-        fisica: ["pedro","lucia","maria","perico","pajaro"],
-        seguridad: ["carmen","sofia","laura"]
+        deporte: ["pedro","juan","perico","lucia"],
+        fisica: ["pedro","laura","juan","perico"],
+        seguridad: ["carmen","lucia","laura","juan"]
     }
 
     if (materia !== undefined){
@@ -10,7 +10,7 @@ const obtenerInformacion = (materia)=>{
     }
 }
 
-    let informacion = obtenerInformacion("seguridad")
+    let informacion = obtenerInformacion(materias)
     // let alumnos = informacion[0].join(", "); // Une los nombres de los alumnos separados por comas
     let asignatura = informacion[1];
     let profesor = informacion[0].shift();
@@ -19,4 +19,23 @@ const obtenerInformacion = (materia)=>{
     document.write(
         `La materia es: ${asignatura}<br>
         Profesor: ${profesor}<br>
-        Alumnos: ${alumnos}`);
+        Alumnos: ${alumnos}<br><br>`);
+
+
+
+const cantidadDeClases = (alumno) => {
+    var informacion2 = obtenerInformacion();
+    let cantidadTotal = 0;
+    let clasesAsistiendo = [];
+    for (info in informacion2){
+        if (informacion2[info].includes(alumno)){
+            cantidadTotal++;
+            clasesAsistiendo.push(" "+ info)
+        }
+    }
+    return `${alumno} está en ${cantidadTotal} y son: ${clasesAsistiendo}.<br>`
+}
+document.write(cantidadDeClases("lucia"));
+document.write(cantidadDeClases("juan"));
+document.write(cantidadDeClases("perico"));
+document.write(cantidadDeClases("laura"));
