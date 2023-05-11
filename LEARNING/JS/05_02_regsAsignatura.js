e1bd8514a9ea7e7924f9d72159e9f84996f37673
blkshhh <er.miguel.ag@gmail.com>
2023-05-11 19:23:04 +0200

    11/05/23 - 19:22

--- LEARNING/JS/05_02_regsAsignatura.js
+++ LEARNING/JS/05_02_regsAsignatura.js
@@ -1,42 +1,48 @@
+// Array con la información de las asignaturas y los alumnos
 const obtenerInformacion = [
-    "deporte",
-    ["pedro", "juan", "perico", "lucia"],
-    "fisica",
-    ["pedro", "laura", "juan", "perico"],
-    "seguridad",
-    ["carmen", "lucia", "laura", "juan"],
-  ];
-  
-  const mostrarInfo = (materia) => {
-    let asignatura;
-    let alumnos;
-  
-    for (let i = 0; i < obtenerInformacion.length; i += 2) {
-      if (obtenerInformacion[i] === materia) {
-        asignatura = obtenerInformacion[i];
-        alumnos = obtenerInformacion[i + 1].join(", ");
-        break;
-      }
-    }
-  
-    if (asignatura && alumnos) {
-      let profesor = obtenerInformacion[1];
-      document.write(
-        `La materia es: ${asignatura}<br>
-        Profesor: ${profesor}<br>
-        Alumnos: ${alumnos}<br><br>`
-      );
-    } else {
-      document.write("La materia seleccionada no existe.<br><br>");
-    }
-  };
-  
-  // Mostrar todas las asignaturas
-  document.write("Asignaturas disponibles:<br>");
+  "deporte",                    // Asignatura
+  ["pedro", "juan", "perico", "lucia"],  // Alumnos
+  "fisica",                     // Asignatura
+  ["pedro", "laura", "juan", "perico"],  // Alumnos
+  "seguridad",                  // Asignatura
+  ["carmen", "lucia", "laura", "juan"]    // Alumnos
+];
+
+// Función para mostrar la información de una materia específica
+const mostrarInfo = (materiaAComprobar, alumnoAComprobar) => {
+  let asignatura;   // Variable para almacenar la asignatura encontrada
+  let alumnos;      // Variable para almacenar la lista de alumnos
+
+  // Recorrer el array de información
   for (let i = 0; i < obtenerInformacion.length; i += 2) {
-    document.write(obtenerInformacion[i] + "<br>");
+    if (obtenerInformacion[i] === materiaAComprobar) {   // Si la asignatura coincide
+      asignatura = obtenerInformacion[i];      // Almacenar la asignatura
+      alumnos = obtenerInformacion[i + 1].join(", ");  // Unir los alumnos en una cadena separada por comas
+      break;  // Salir del bucle
+    }
   }
-  
-  let materia = prompt("Dime la materia");
-  mostrarInfo(materia);
-  
+
+  if (asignatura && alumnos) {   // Si se encontró la asignatura y la lista de alumnos
+    let indiceAsignatura = obtenerInformacion.indexOf(asignatura);   // Obtener el índice de la asignatura
+    let profesor = obtenerInformacion[indiceAsignatura + 1][0];      // Obtener el nombre del profesor
+    let contadorAlumnos = obtenerInformacion[indiceAsignatura + 1].length; // Obtener el número de alumnos
+
+    document.write(
+      `La materia es: ${asignatura}<br>
+      Profesor: ${profesor}<br>
+      Alumnos: ${alumnos}<br>
+      Total de Alumnos: ${contadorAlumnos}<br><br>`
+    );
+  } else {
+    document.write("No se encontró información para la materia ingresada.<br><br>");
+  }
+};
+
+// Mostrar todas las asignaturas disponibles
+let listadoAsignaturas = obtenerInformacion.filter((element, index) => index % 2 === 0).join(", ");
+document.write(`Las asignaturas disponibles son: ${listadoAsignaturas}.<br><br>`);
+
+// Solicitar al usuario una materia y un alumno
+let materiaAComprobar = prompt("Dime la materia");
+let alumnoAComprobar = prompt("Qué alumno quieres ver si está inscrito");
+mostrarInfo(materiaAComprobar, alumnoAComprobar);  // Mostrar la información de la materia ingresada
