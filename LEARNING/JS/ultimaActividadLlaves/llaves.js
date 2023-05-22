748c4029360466cab055a16107ca066896247384
blkshhh <er.miguel.ag@gmail.com>
2023-05-22 13:56:12 +0200

    22/05/23 - 13:56

--- LEARNING/JS/ultimaActividadLlaves/llaves.js
+++ LEARNING/JS/ultimaActividadLlaves/llaves.js
@@ -1,24 +1,22 @@
 var contenedor = document.querySelector(".flex-container");
 
-function crearLlave(nombre,modelo,precio) {
+function crearLlave(nombre, modelo, precio) {
     let img = `<img src='./llave.png'>`;
     nombre = `<h2>${nombre}</h2>`;
     modelo = `<h3 id="${modelo}">${modelo}</h3>`;
     precio = `<p>$${precio}</p>`;
 
-    return [img,nombre,modelo,precio];
+    return [img, nombre, modelo, precio];
 }
-
-/////////////////////////////////////////////////////////////////////
 
 let fragmentoDeDocumento = document.createDocumentFragment();
 
 for (var i = 1; i <= 20; i++) {
     let modeloRandom = Math.round(Math.random() * 10000);
     let precioRandom = Math.round(Math.random() * 10 + 30);
-    let llave = crearLlave(`Llave ${i}`,`Modelo: ${modeloRandom}`,`${precioRandom}`);
+    let llave = crearLlave(`Llave ${i}`, `Modelo: ${modeloRandom}`, `${precioRandom}`);
     let div = document.createElement("DIV");
-    div.classList.add(`item-flex`,`item-${i}`);
+    div.classList.add(`item-flex`, `item-${i}`);
     div.innerHTML = llave.join("");
     fragmentoDeDocumento.appendChild(div);
 }
