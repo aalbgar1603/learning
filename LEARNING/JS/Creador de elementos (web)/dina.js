374d5b9eb2bf8ef4d3d5732edd4e1685a938d8dd
blkshhh <er.miguel.ag@gmail.com>
2023-05-24 18:41:29 +0200

    24/05/23 - 18:40 - Practicando la creacion de elementos en el DOM con JS

---
+++ LEARNING/JS/Creador de elementos (web)/dina.js
@@ -0,0 +1,41 @@
+class Crear {
+    constructor() {};
+    elemento(){
+        var mensaje = document.getElementById("mensajeInput").value;
+        var etiqueta = document.getElementById("etiquetaInput").value;
+        var contenedor = document.getElementById("contenedor");
+        if (etiqueta == "li" || etiqueta == "ol" || etiqueta == "ul") {
+            var contenido = document.createElement("UL");
+            let li = document.createElement("LI");
+            li.innerHTML = mensaje;
+            contenido.appendChild(li);
+        }
+        var contenido = document.createElement(etiqueta);
+        contenido.innerHTML = mensaje;
+        contenedor.appendChild(contenido)
+    }
+    h1(){
+        this.mensaje = document.getElementById("inputText").value;
+        var contenedor = document.getElementById("contenedor");
+        var contenido = document.createElement("H1");
+        contenido.innerHTML = this.mensaje;
+        contenedor.appendChild(contenido)
+    }
+    h2(){
+        this.mensaje = document.getElementById("inputText").value;
+        var contenedor = document.getElementById("contenedor");
+        var contenido = document.createElement("H2");
+        contenido.innerHTML = this.mensaje;
+        contenedor.appendChild(contenido)
+    }
+    h3(){
+        this.mensaje = document.getElementById("inputText").value;
+        var contenedor = document.getElementById("contenedor");
+        var contenido = document.createElement("H3");
+        contenido.innerHTML = this.mensaje;
+        contenedor.appendChild(contenido)
+        
+    }
+}
+
+var creador = new Crear();
