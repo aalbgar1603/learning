c051e6f70b604967e452e8432692808b29d7de97
blkshhh <er.miguel.ag@gmail.com>
2023-05-26 21:51:52 +0200

    mmm

--- LEARNING/JS/ultimaActividadLlaves/llaves.js
+++ LEARNING/JS/ultimaActividadLlaves/llaves.js
@@ -1,3 +1,4 @@
+// seleccionamos el contenedor donde iremos metiendo cada llave
 var contenedor = document.querySelector(".flex-container");
 
 function crearLlave(nombre,modelo,precio) {
@@ -10,6 +11,13 @@ function crearLlave(nombre,modelo,precio) {
 }
 
 /////////////////////////////////////////////////////////////////////
+// Creamos un fragmento del documento index para poder devolver codigo
+// que pueda interpretar el navegador. 
+// Porque si intentaramos añadir cada div al document haciendo un .innetHTML
+// este añadiría los tipos de elementos que estamos metiendo y no interpretaria
+// el codigo html. 
+// Por esa razón, al final del for, introducimos cada div dentro del fragmento
+// asignandolos como hijos de este con .appendChild.
 
 let fragmentoDeDocumento = document.createDocumentFragment();
 
@@ -18,8 +26,9 @@ for (var i = 1; i <= 20; i++) {
     let precioRandom = Math.round(Math.random() * 10 + 30);
     let llave = crearLlave(`Llave ${i}`,`Modelo: ${modeloRandom}`,`${precioRandom}`);
     let div = document.createElement("DIV");
+    div.tabIndex = i;
     div.classList.add(`item-flex`,`item-${i}`);
-    div.innerHTML = llave.join("");
+    div.innerHTML = llave.join(""); //para que ignore las comas. 
     fragmentoDeDocumento.appendChild(div);
 }
 
