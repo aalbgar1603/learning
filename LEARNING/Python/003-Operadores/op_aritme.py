25a948e5cde52285b7f40a55d8be50302c9e9f34
blkshhh <er.miguel.ag@gmail.com>
2023-05-29 22:14:39 +0200

    29/05/2023 - 22:14 +

---
+++ LEARNING/Python/003-Operadores/op_aritme.py
@@ -0,0 +1,7 @@
+suma = 1 + 1
+resta = 1 - 1
+multi = 2 * 2
+division = 5 / 5 #Devuelve un float
+exponente = 12 ** 10
+divisionBaja = 12 // 5 # Redondea hacia abajo y devuelve un string
+resto =  5 % 3
